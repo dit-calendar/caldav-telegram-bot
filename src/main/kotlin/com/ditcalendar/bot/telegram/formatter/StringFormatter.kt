068035899f3548dae4b202ba0c7606bc44167acd
Viktor Befort <v.befort@googlemail.com>
2020-11-27 22:35:32 +0100

    improve error message

--- src/main/kotlin/com/ditcalendar/bot/telegram/formatter/StringFormatter.kt
+++ src/main/kotlin/com/ditcalendar/bot/telegram/formatter/StringFormatter.kt
@@ -2,6 +2,7 @@ package com.ditcalendar.bot.telegram.formatter
 
 import com.ditcalendar.bot.domain.data.DitBotError
 import org.apache.jackrabbit.webdav.DavException
+import java.lang.IllegalArgumentException
 import java.net.UnknownHostException
 
 const val reloadButtonText = "reload"
@@ -11,8 +12,16 @@ fun parseErrorToString(error: Exception): String =
         when (error) {
             is DitBotError -> error.message!!
             else -> when (error.cause) {
-                is DavException -> "parsing error for calDav response. Please check your calDav url or server"
+                is DavException -> parsingError
                 is UnknownHostException -> "wrong calDav url"
-                else -> "unknown error"
+                is IllegalArgumentException ->
+                    if((error.cause as IllegalArgumentException).message == "DAV:multistatus element expected.")
+                        parsingError
+                    else
+                        unknownError
+                else -> unknownError
             }
         }.withMDEscape()
+
+private const val parsingError = "parsing error for calendar. Please check the CalDav url or credentials"
+private const val unknownError = "unknown error"
